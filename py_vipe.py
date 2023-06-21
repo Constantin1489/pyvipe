8d51de6802530ef62034e71830b33e1c5fe93e0a
Constantin Hong <hongconstantin@gmail.com>
2023-06-22 08:06:00 +0900

    py_vipe/refactor: Remove suffix and reorder code.

--- py_vipe.py
+++ py_vipe.py
@@ -15,17 +15,13 @@ if 'VISUAL' in os.environ:
 text = sys.stdin.read()
 initial_message = text.encode()
 
-with tempfile.NamedTemporaryFile(suffix=".tmp") as tf: 
+stdin_fd = os.open('/dev/tty', os.O_RDONLY)
+os.dup2(stdin_fd, 0)
+os.close(stdin_fd)
+
+with tempfile.NamedTemporaryFile() as tf:
     tf.write(initial_message)
     tf.flush()
-
-    stdin_fd = os.open('/dev/tty', os.O_RDONLY)
-    os.dup2(stdin_fd, 0)
-    os.close(stdin_fd)
-
-#    stdout_fd = os.open('/dev/tty', os.O_WRONLY)
-#    os.dup2(stdout_fd, 0)
-#    os.close(stdout_fd)
 
     subprocess.check_call([EDITOR, tf.name])
     print(open(tf.name).read(), end='')
